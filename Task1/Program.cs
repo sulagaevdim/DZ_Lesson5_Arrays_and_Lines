bb06bc6f45b38613912153511ec5e23e384f2972
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-28 00:21:30 +0400

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,] chars_matrix = {
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f'},
+    {'h', 'j', 'k'}
+};
+foreach (char chars in chars_matrix)
+{
+    System.Console.Write(chars);    
+}
