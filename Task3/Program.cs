b93b8a45cfe456bd7a6d8a4a2da422b316e99b04
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-28 01:01:40 +0400

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string str = "шаввашшовваш";
+bool result = false;
+for (int i = 0; i < (str.Length / 2); i++)
+{
+    if (str[i] == str[str.Length - 1 - i])
+    {
+        result = true;
+    }
+    else {
+        result = false;
+        break;
+    }
+}
+if (result) {System.Console.WriteLine("Да");}
+else {System.Console.WriteLine("Нет");}
